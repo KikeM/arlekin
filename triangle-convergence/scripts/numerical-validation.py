e1414b4ba10623ca439b4d4f101d60f23ee42764
Enrique Millan Valbuena <enrique.millanvalbuena@gmail.com>
2023-08-19 22:37:32 +0200

    feat(scripts): check convergence

--- triangle-convergence/scripts/numerical-validation.py
+++ triangle-convergence/scripts/numerical-validation.py
@@ -1,5 +1,3 @@
-from dataclasses import dataclass
-
 import matplotlib.pyplot as plt
 import numpy as np
 from rich import print
@@ -19,7 +17,7 @@ class Point:
     def __repr__(self) -> str:
         return f"Point({self.x}, {self.y})"
 
-    def __eq__(self, other: object) -> bool:
+    def __eq__(self, other: "Point") -> bool:
         if not isinstance(other, Point):
             return NotImplemented
 
@@ -34,9 +32,8 @@ class Point:
     def length_squared(self) -> float:
         return self.x**2 + self.y**2
 
-
-def ccw(A: Point, B: Point, C: Point) -> bool:
-    return (C.y - A.y) * (B.x - A.x) > (B.y - A.y) * (C.x - A.x)
+    def distance(self, other: "Point") -> float:
+        return np.sqrt((self.x - other.x) ** 2 + (self.y - other.y) ** 2)
 
 
 class Triangle:
@@ -78,6 +75,12 @@ class Triangle:
 
         return Point(Ux, Uy)
 
+    def center_of_mass(self) -> Point:
+        return Point(
+            (self.A.x + self.B.x + self.C.x) / 3,
+            (self.A.y + self.B.y + self.C.y) / 3,
+        )
+
     def midpoints(self) -> list[Point]:
         return [
             Point((self.A.x + self.B.x) / 2, (self.A.y + self.B.y) / 2),
@@ -95,11 +98,11 @@ def plot_point(ax: plt.Axes, point: Point, color: str = "black"):
     ax.scatter(point.x, point.y, color=color)
 
 
-def plot_triangle(ax: plt.Axes, triangle: Triangle):
+def plot_triangle(ax: plt.Axes, triangle: Triangle, **kwargs):
     ax.plot(
         [triangle.A.x, triangle.B.x, triangle.C.x, triangle.A.x],
         [triangle.A.y, triangle.B.y, triangle.C.y, triangle.A.y],
-        color="black",
+        **kwargs,
     )
 
 
@@ -131,25 +134,59 @@ if __name__ == "__main__":
     test_point_equivalence()
     test_circumcentre()
 
-    _, ax = plt.subplots()
+    figsize = (8, 8)
+    _, axes = plt.subplots(nrows=2, figsize=figsize)
+
+    axes = axes.flatten()
 
     A = Point(0, 0)
-    B = Point(1, 0)
-    C = Point(0, 1)
+    B = Point(4, 3.9)
+    C = Point(0, 7)
 
     triangle = Triangle(A, B, C)
-    circumcentre = triangle.circumcentre()
-    midpoints = triangle.midpoints()
+
+    ax = axes[0]
 
     plot_point(ax, A)
     plot_point(ax, B)
     plot_point(ax, C)
-
-    for midpoint in midpoints:
-        plot_point(ax, midpoint, color="green")
-
     plot_triangle(ax, triangle)
 
+    # Plot center of mass
+    center_of_mass = triangle.center_of_mass()
+    plot_point(ax, center_of_mass, color="red")
+
+    N = 25
+
+    previous_triangle = triangle
+    distances = []
+    for idx in range(N):
+        midpoints = previous_triangle.midpoints()
+        triangle_new = Triangle(midpoints[0], midpoints[1], midpoints[2])
+
+        circumcentre = triangle_new.circumcentre()
+        distance = circumcentre.distance(center_of_mass)
+        distances.append(distance)
+
+        plot_triangle(
+            ax,
+            triangle_new,
+            color="black",
+            linewidth=0.5,
+        )
+        previous_triangle = triangle_new
+
+    ax.set_title("Nagore's Point")
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+
+    ax = axes[1]
+
+    ax.semilogy(range(N), distances, marker="o")
+    ax.set_title("Distance circumcentres to center of mass")
+    ax.set_xlabel("Iteration")
+    ax.set_ylabel("Distance")
     ax.grid()
 
+    plt.tight_layout()
     plt.show()
